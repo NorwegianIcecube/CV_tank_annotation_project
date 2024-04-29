8cd9ea414ce14784b97136778061f4e768f6dada
Patrick Karlsen <patkar07.pk@gmail.com>
2024-04-29 16:43:53 +0200

    add script to segment video data to img data

---
+++ image_seperator.py
@@ -0,0 +1,40 @@
+import cv2
+import os
+
+video_path = './data/tank_videos'
+
+image_path = './data/tank_images'
+
+def get_frames(video, frame_interval):
+    # Get the video
+    cap = cv2.VideoCapture(video)
+
+    number_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Get the frames
+    frames = []
+    for i in range(number_of_frames):
+        ret, frame = cap.read()
+        if ret and i % frame_interval == 0:
+            frames.append(frame)
+    cap.release()
+    return frames
+
+def save_frames(frames, video_name):
+    for i, frame in enumerate(frames):
+        cv2.imwrite(f'{image_path}/{video_name}/frame_{i+1}.jpg', frame)
+
+def main():
+    videos = os.listdir(video_path)
+    videos.remove('.gitkeep')
+
+    for video in videos:
+        try:
+            os.mkdir(f'{image_path}/{video}')
+        except FileExistsError:
+            pass
+        frames = get_frames(f'{video_path}/{video}', 1)
+        save_frames(frames, video)
+
+if __name__ == '__main__':
+    main()
